fa6fde4f3bfe1bd4d9c75debcc97a6315ed902c8
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2023-04-11 16:39:49 -0400

    add

--- src/playlists/observe/display.js
+++ src/playlists/observe/display.js
@@ -4,7 +4,13 @@ import { match } from '../utility/index.js'
 const display = match({
     Show:  ({playlist}) => {
         return playlist;
+    },
+    Add: ({...playlists}) => {
+        let playlist;
+        playlists.forEach(element => {
+            playlist = playlist.concat(element);
+        });
+        return  playlist;
     }
-});
-
+})
 export {display};
