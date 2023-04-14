06c308cce7c1eaad38cb285776e914545cfcd469
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2023-04-14 16:11:15 -0400

    okay, now the filter should work

--- src/index.js
+++ src/index.js
@@ -20,8 +20,8 @@ const playlists = Playlist.Add(testPlaylist1, testPlaylist2);
 const addDisplay = display(playlists);
 
 // TEST FOR FILTER DUPLICATES
-//const dupliDisplay = display(Playlist.FilterDuplicates(testPlaylist1));
-//document.querySelector('#app').innerHTML = renderDisplay(dupliDisplay);
+const dupliDisplay = display(Playlist.FilterDuplicates(testPlaylist1));
+document.querySelector('#app').innerHTML = renderDisplay(dupliDisplay);
 
 //TEST FOR SORTING BY NUMBER OF PLAYS PER PLAYLIST
 let testPlaylist3 = ['Playlist 1', 1, 7, 'song 1', 'song 2', 'song 3', 'song 1'];
@@ -29,4 +29,4 @@ let testPlaylist4 = ['Playlist 2', 2.5, 2, 'song 1', 'song 2', 'song 3'];
 let testPlaylist5 = ['Playlist 3', 2.5, 1, 'song 1', 'song 2', 'song 3'];
 const sortPlaylist = Playlist.PlaylistSort(2)(testPlaylist3, testPlaylist4, testPlaylist5);
 
-document.querySelector('#app').innerHTML = renderDisplay(sortPlaylist);
+// document.querySelector('#app').innerHTML = renderDisplay(sortPlaylist);
--- src/playlists/api.js
+++ src/playlists/api.js
@@ -3,10 +3,10 @@ import {Alg} from './algebra.js';
 const API = {
     Show: (playlist) => Alg.show(playlist),
     Add: (...playlists) => Alg.add(...playlists),
-    // Filter:  (filterReq, playlist) => Alg.filter(filterReq, playlist),
+    Filter:  (filterReq, playlist) => Alg.filter(filterReq, playlist),
     FilterDuplicates: (playlist) =>
     {
-        return Alg.playlistFilter((value,index) => playlist.indexOf(value) == index);
+        return Alg.playlistFilter(((value,index) => playlist.indexOf(value) == index),playlist);
     },
     PlaylistSort: (index) =>  (...playlists) => {
         let sortedObject = Alg.playlistSort(...playlists.sort((a, b) => a[index] - b[index]));
