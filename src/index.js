d46c0c418422e47f048b9f88f6dead0044738ca9
Neha Ghanta <55118296+NehaG991@users.noreply.github.com>
2023-04-14 23:23:30 -0400

    nothing big. just some small reorganizing in index.js for easier testing

--- src/index.js
+++ src/index.js
@@ -9,24 +9,24 @@ let testPlaylist = ['Playlist 1', 1, 5, 'song 1', 'song 2', 'song 3'];
 // TEST FOR SHOW
 const playlist = Playlist.Show(testPlaylist);
 const showDisplay = display(playlist);
+//document.querySelector('#app').innerHTML = renderDisplay(showDisplay);
 
 // TEST FOR ADD
 let testPlaylist1 = ['Playlist 1', 1, 7, 'song 1', 'song 2', 'song 3', 'song 1'];
 let testPlaylist2 = ['Playlist 2', 2.5, 2, 'song 1', 'song 2', 'song 3'];
 
 const playlists = Playlist.Add(testPlaylist1, testPlaylist2);
-//console.log(playlists);
-
 const addDisplay = display(playlists);
+//document.querySelector('#app').innerHTML = renderDisplay(addDisplay);
 
 // TEST FOR FILTER DUPLICATES
-const dupliDisplay = display(Playlist.FilterDuplicates(testPlaylist1));
-document.querySelector('#app').innerHTML = renderDisplay(dupliDisplay);
+let testPlaylist6 = ['Playlist 1', 1, 7, 'song 1', 'song 2', 'song 3', 'song 1'];
+const dupliDisplay = display(Playlist.FilterDuplicates(testPlaylist6));
+//document.querySelector('#app').innerHTML = renderDisplay(dupliDisplay);
 
 //TEST FOR SORTING BY NUMBER OF PLAYS PER PLAYLIST
 let testPlaylist3 = ['Playlist 1', 1, 7, 'song 1', 'song 2', 'song 3', 'song 1'];
 let testPlaylist4 = ['Playlist 2', 2.5, 2, 'song 1', 'song 2', 'song 3'];
 let testPlaylist5 = ['Playlist 3', 2.5, 1, 'song 1', 'song 2', 'song 3'];
 const sortPlaylist = Playlist.PlaylistSort(2)(testPlaylist3, testPlaylist4, testPlaylist5);
-
-// document.querySelector('#app').innerHTML = renderDisplay(sortPlaylist);
+//document.querySelector('#app').innerHTML = renderDisplay(sortPlaylist);
